0f5be0118fba41d7a1df048a6f1faa232ea12499
YashedBin <yash567pisudde@gmail.com>
2026-02-17 23:33:21 +0530

    feat(migrate): add Core::FileLoader
    - Added FileLoader Core Utility to load files

--- client/include/core/fileloader.hpp
+++ client/include/core/fileloader.hpp
@@ -1,17 +1,22 @@
 #pragma once
 
 #include <fstream>
+#include <vector>
 #include <string>
 #include <iostream>
 
 
-class FileLoader {
-    private:
-        std::string path;
+namespace Core {
 
-    public:
-        FileLoader();
-        virtual ~FileLoader();
-        void loadLines();
+    class FileLoader {
+        private:
+            std::ifstream m_file;
+
+        public:
+            explicit FileLoader(const char* path);
+            virtual ~FileLoader();
+            std::vector<std::string> loadLines();
+            std::vector<std::vector<char>> loadGrid();
+    };
 
 }
