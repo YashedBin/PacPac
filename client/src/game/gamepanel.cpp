f3a2294be21ae78369e22a5b47258e289d65f4e4
YashedBin <yash567pisudde@gmail.com>
2026-02-17 23:56:01 +0530

    refact(migrate): Refactor for ImGui-SFML & Tested FileLoader

--- client/src/game/gamepanel.cpp
+++ client/src/game/gamepanel.cpp
@@ -2,8 +2,16 @@
 #include <iostream>
 #include <string>
 
+#include "imgui.h"
+#include "imgui-SFML.h"
+#include <SFML/System/Clock.hpp>
+#include <SFML/Window/Event.hpp>
+#include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+
 #include "game/gamepanel.hpp"
 #include "game/global.hpp"
+#include "game/map.hpp"
 
 
 namespace Game {
@@ -16,29 +24,51 @@ sf::RenderWindow Gamepanel::window(
 Engine::ConsoleLog Gamepanel::console("PacPac");
 
 Gamepanel::Gamepanel() {
+    window.setFramerateLimit(Global::FPS);
+    //ImGui::SFML::Init(window, true);
+    if (!ImGui::SFML::Init(window, true)) {
+        std::cerr << "Failed to Init ImGui::SFML" << std::endl;
+        return;
+    }
 
 }
 
+
+
+
 void Gamepanel::Run() {
     
-    while (isRunning())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
+    sf::Clock deltaClock;
+    while (isRunning()) {
+
+        while (const auto event = window.pollEvent()) {
+
+            ImGui::SFML::ProcessEvent(window, *event);
             if(event->is<sf::Event::Closed>())
                 window.close();
         }
+        
+        ImGui::SFML::Update(window, deltaClock.restart());
+        ImGui::ShowDemoWindow();
+
+        ImGui::Begin("Is it a HEllow Worls?");
+        ImGui::Button("Don't tab on me!!");
+        ImGui::End();
         Render();
     } 
 }
 
+
 Gamepanel::~Gamepanel() {
 
+    ImGui::SFML::Shutdown();
 }
+
 
 bool Gamepanel::isRunning() {
     return window.isOpen();
 }
+
 
 void Gamepanel::Render() {
 
@@ -46,8 +76,10 @@ void Gamepanel::Render() {
     square.setFillColor(sf::Color::Green);
     square.setPosition(sf::Vector2f(350.f, 250.f));
     
-    window.clear(sf::Color::Black);
+    window.clear();
     window.draw(square);
+
+    ImGui::SFML::Render(window);
     window.display();
 
 }
