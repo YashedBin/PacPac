2983ac12f5cd317f4339934c876dd92687348e8b
YashedBin <yash567pisudde@gmail.com>
2026-02-24 17:47:57 +0530

    feat(pacpac): add Core::Timer class & removed timer.cpp
    - Fixed indentation of files -> fileloader.cpp , timer.hpp

--- client/src/core/fileloader.cpp
+++ client/src/core/fileloader.cpp
@@ -6,42 +6,42 @@
 
 namespace Core {
     
-FileLoader::FileLoader(const char* path)
-    : m_file(path) {
-        // default neg
-        if(!m_file.is_open()) {
-            throw std::runtime_error("Failed to open File:" + std::string(path));
+    FileLoader::FileLoader(const char* path)
+        : m_file(path) {
+            // default neg
+            if(!m_file.is_open()) {
+                throw std::runtime_error("Failed to open File:" + std::string(path));
+            }
+    }
+
+
+    FileLoader::~FileLoader() {
+        // Check for Open File
+        if (m_file.is_open()) {
+            m_file.close();
         }
     }
 
 
-FileLoader::~FileLoader() {
-    // Check for Open File
-    if (m_file.is_open()) {
-        m_file.close();
-    }
-}
+    std::vector<std::string> FileLoader::loadLines() {
+        std::vector<std::string> dataLines;
+        
+        std::string line;
 
+        while(std::getline(m_file, line)) {
+            dataLines.push_back(line);
+        }
 
-std::vector<std::string> FileLoader::loadLines() {
-    std::vector<std::string> dataLines;
-    
-    std::string line;
+        return dataLines;
 
-    while(std::getline(m_file, line)) {
-        dataLines.push_back(line);
     }
 
-    return dataLines;
+    std::vector<std::vector<char>> FileLoader::loadGrid() {
+        std::vector<std::vector<char>> dataGrid;
 
-}
-
-std::vector<std::vector<char>> FileLoader::loadGrid() {
-    std::vector<std::vector<char>> dataGrid;
-
-    char c;
+        char c;
     
-    return dataGrid;
-}
+        return dataGrid;
+    }
 
 }
