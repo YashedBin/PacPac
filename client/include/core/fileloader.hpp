654c72a0626bcaaa6444f713bd2126657b0db303
YashedBin <yash567pisudde@gmail.com>
2026-02-26 23:59:59 +0530

    feat(migrate): revamped FileLoader with auto detect file loading
    - .png , .txt supported

--- client/include/core/fileloader.hpp
+++ client/include/core/fileloader.hpp
@@ -4,6 +4,9 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <variant>
+
+#include <SFML/Graphics/Image.hpp>
 
 
 namespace Core {
@@ -11,12 +14,14 @@ namespace Core {
     class FileLoader {
         private:
             std::ifstream m_file;
+            sf::Image img;
 
         public:
             explicit FileLoader(const char* path);
             virtual ~FileLoader();
-            std::vector<std::string> loadLines();
-            std::vector<std::vector<char>> loadGrid();
+            std::vector<std::string> loadLines(const char*);
+            static std::variant<sf::Image, std::vector<std::string>, std::string> load(const char* filepath);
+            // json , yaml later 
+            std::pair<std::string, std::string> FileLoader::getPrefixSuffix(const char* path);
     };
-
 }
--- client/src/core/fileloader.cpp
+++ client/src/core/fileloader.cpp
@@ -23,7 +23,8 @@ namespace Core {
     }
 
 
-    std::vector<std::string> FileLoader::loadLines() {
+    std::vector<std::string> FileLoader::loadLines(const char* path) {
+        std::ifstream m_file(path);
         std::vector<std::string> dataLines;
         
         std::string line;
@@ -36,12 +37,55 @@ namespace Core {
 
     }
 
-    std::vector<std::vector<char>> FileLoader::loadGrid() {
-        std::vector<std::vector<char>> dataGrid;
+    sf::Image loadImage(const char* path) {
+        if(!img.loadFromFile(path)){
+            throw std::runtime_error("Failed to open File:" + std::string(path));
+        }
+        return img;        
+    }
 
-        char c;
-    
-        return dataGrid;
+    std::variant<sf::Image, std::vector<std::string>, std::string> FileLoader::load(const char* filepath) {
+        std::pair<std::string, std::string> preSuf = getPrefixSuffix(filepath);
+            
+        
+            if (preSuf.second == ".png" && preSuf.first == "ani_") {
+                return loadImage(filepath);
+                // for SpriteSheets
+            }
+
+            if (preSuf.second == ".png") {
+                // return loadImage(path);
+                // for Temps or Logos
+            }
+
+            if (preSuf.second == ".txt" && preSuf.first == "map_") {
+                return loadLines(filepath);
+            }
+
+            if (preSuf.second == ".bin" && preSuf.first == "map_") {
+                // maps in binary
+                // return loadBinary(filepath);
+            }
+
+            if(preSuf.second == ".txt" && preSuf.first != "map_") {
+                // normal txt so far
+            }
+
+            throw std::runtime_error("Unsupported file type"); 
+    }
+
+    std::pair<std::string, std::string> FileLoader::getPrefixSuffix(const char* path) {
+        std::string fpath = std::string(path);
+        std::string prefix = fpath.substr(0,4);
+        /*
+         * All Assets are suppose to have 3 len Prefix 
+         * describing their Main Use
+         */
+        size_t pos = fpath.find_last_of('.');
+        if (pos == std::string::npos) return {"", ""};
+        std::string suffix = fpath.substr(pos);   
+        
+        return {prefix,suffix};
     }
 
 }
