178cbb9a72827fc7522300b48387fdaf09d89dd0
YashedBin <yash567pisudde@gmail.com>
2026-03-04 23:56:37 +0530

    config(migrate): update build.bat for testing Windows

--- build.bat
+++ build.bat
@@ -1,15 +1,10 @@
 @echo off
-REM Batch for building Client 
-REM Windows is so different
 
-REM Creates obviously a Build Directory 
 if not exist build-win mkdir build-win
 cd build-win
 
-
 cmake .. -G "Visual Studio 17 2022" -A x64
-
 cmake --build . --config Release
 
-REM Running the Exec
-REM pacpac.exe
+cd ..
+build-win\bin\Release\pacpac.exe
